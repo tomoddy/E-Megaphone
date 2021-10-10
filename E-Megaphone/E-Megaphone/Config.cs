f86a25b3d00f50131994ca659242c96e0fa18ae7
Tom Oddy <tom_oddy@live.co.uk>
2021-10-10 12:29:21 +0100

    Added documentation

--- E-Megaphone/E-Megaphone/Config.cs
+++ E-Megaphone/E-Megaphone/Config.cs
@@ -2,33 +2,54 @@
 
 namespace E_Megaphone
 {
+    /// <summary>
+    /// Email client configuration values
+    /// </summary>
     public static class Config
     {
+        /// <summary>
+        /// Name of sender
+        /// </summary>
         public static string Sender
         {
             get { return ConfigurationManager.AppSettings.Get("Sender"); }
         }
 
+        /// <summary>
+        /// Email address of sender
+        /// </summary>
         public static string Username
         {
             get { return ConfigurationManager.AppSettings.Get("Username"); }
         }
 
+        /// <summary>
+        /// Password of sender
+        /// </summary>
         public static string Password
         {
             get { return ConfigurationManager.AppSettings.Get("Password"); }
         }
 
+        /// <summary>
+        /// Email message contect subtype (text/SubType)
+        /// </summary>
         public static string SubType
         {
             get { return ConfigurationManager.AppSettings.Get("SubType"); }
         }
 
+        /// <summary>
+        /// SMTP server hostname
+        /// </summary>
         public static string Host
         {
             get { return ConfigurationManager.AppSettings.Get("Host"); }
         }
 
+        /// <summary>
+        /// SMTP server port
+        /// </summary>
         public static int Port
         {
             get { return int.Parse(ConfigurationManager.AppSettings.Get("Port")); }
--- E-Megaphone/E-Megaphone/Mailer.cs
+++ E-Megaphone/E-Megaphone/Mailer.cs
@@ -1,35 +1,66 @@
 ﻿using MailKit.Net.Smtp;
 using MailKit.Security;
 using MimeKit;
+using System;
 using System.Collections.Generic;
 
 namespace E_Megaphone
 {
+    /// <summary>
+    /// Mailer class used to send emails
+    /// </summary>
     class Mailer
     {
-        public void Send(string subject, string text, Dictionary<string, string> recipients)
+        /// <summary>
+        /// Send email
+        /// </summary>
+        /// <param name="subject">Email subject</param>
+        /// <param name="text">Email body</param>
+        /// <param name="recipients">Dictionary of recipients (email key and name value)</param>
+        /// <returns>Status code (0 = success, -1 = failure)</returns>
+        public int Send(string subject, string text, Dictionary<string, string> recipients)
         {
-            MimeMessage message = new MimeMessage
+            try
             {
-                Subject = subject,
-                Body = new TextPart(Config.SubType)
+                // Create email with subject and body
+                MimeMessage message = new MimeMessage
                 {
-                    Text = text
+                    Subject = subject,
+                    Body = new TextPart(Config.SubType)
+                    {
+                        Text = text
+                    }
+                };
+
+                // Add sender information
+                message.From.Add(new MailboxAddress(Config.Sender, Config.Username));
+
+                // Add receipients
+                foreach (KeyValuePair<string, string> recipient in recipients)
+                {
+                    message.To.Add(new MailboxAddress(recipient.Value, recipient.Key));
                 }
-            };
 
-            message.From.Add(new MailboxAddress(Config.Sender, Config.Username));
-            foreach (KeyValuePair<string, string> recipient in recipients)
-            {
-                message.To.Add(new MailboxAddress(recipient.Value, recipient.Key));
+                // Create smtp client
+                using (SmtpClient smtpClient = new SmtpClient())
+                {
+                    // Connect to server and authenticate
+                    smtpClient.Connect(Config.Host, Config.Port, SecureSocketOptions.StartTls);
+                    smtpClient.Authenticate(Config.Username, Config.Password);
+
+                    // Send message and disconnect from server
+                    smtpClient.Send(message);
+                    smtpClient.Disconnect(true);
+                }
+
+                // Return success status code
+                return 0;
             }
-
-            using (SmtpClient smtpClient = new SmtpClient())
+            catch(Exception ex)
             {
-                smtpClient.Connect(Config.Host, Config.Port, SecureSocketOptions.StartTls);
-                smtpClient.Authenticate(Config.Username, Config.Password);
-                smtpClient.Send(message);
-                smtpClient.Disconnect(true);
+                // Log error and return error status code
+                Console.WriteLine(ex);
+                return -1;
             }
         }
     }
